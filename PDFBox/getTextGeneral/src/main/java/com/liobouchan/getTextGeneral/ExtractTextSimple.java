6994692295627ae9ec1b53f3727a7c62c8f69a67
Leonardo <liogothicvampire@gmail.com>
2018-11-01 12:49:25 -0600

    Read with PdfBox

--- PDFBox/getTextGeneral/src/main/java/com/liobouchan/getTextGeneral/ExtractTextSimple.java
+++ PDFBox/getTextGeneral/src/main/java/com/liobouchan/getTextGeneral/ExtractTextSimple.java
@@ -1,16 +1,25 @@
 package com.liobouchan.getTextGeneral;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 /**
  * Hello world!
  *
  */
 public class ExtractTextSimple 
-{
-    public static void main( String[] args )
+{	
+    public static void main( String[] args ) throws IOException
     {
+    	File file = new File("src/main/sources/regulacion.pdf");
+    	PDDocument document = PDDocument.load(file);
+    	PDFTextStripper pdfStripper = new PDFTextStripper();
+    	String text = pdfStripper.getText(document);
+    	System.out.println(text);
+    	
+    	document.close();
     }
 }
