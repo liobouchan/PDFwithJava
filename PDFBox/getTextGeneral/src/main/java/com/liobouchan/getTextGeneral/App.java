7a8ffec6dd62130115ea5a9f23803487b7f06552
Leonardo <liogothicvampire@gmail.com>
2018-11-01 09:43:36 -0600

    Start the project

---
+++ PDFBox/getTextGeneral/src/main/java/com/liobouchan/getTextGeneral/App.java
@@ -0,0 +1,13 @@
+package com.liobouchan.getTextGeneral;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
