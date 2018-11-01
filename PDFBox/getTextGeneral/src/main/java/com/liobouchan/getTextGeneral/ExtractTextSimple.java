416b84c4a95cee7733270dd924643162a2d8ca0d
Leonardo <liogothicvampire@gmail.com>
2018-11-01 10:04:12 -0600

    Solve Alert

--- PDFBox/getTextGeneral/src/main/java/com/liobouchan/getTextGeneral/ExtractTextSimple.java
+++ PDFBox/getTextGeneral/src/main/java/com/liobouchan/getTextGeneral/ExtractTextSimple.java
@@ -1,13 +1,16 @@
 package com.liobouchan.getTextGeneral;
 
+import java.io.File;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class ExtractTextSimple 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
     }
 }
