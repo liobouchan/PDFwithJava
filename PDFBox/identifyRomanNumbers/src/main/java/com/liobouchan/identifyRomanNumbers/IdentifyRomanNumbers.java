eed2f557115b808811dfb8b1e0a78be7f630e74c
Leonardo <liogothicvampire@gmail.com>
2018-11-13 17:05:07 -0600

    Finished

--- PDFBox/identifyRomanNumbers/src/main/java/com/liobouchan/identifyRomanNumbers/IdentifyRomanNumbers.java
+++ PDFBox/identifyRomanNumbers/src/main/java/com/liobouchan/identifyRomanNumbers/IdentifyRomanNumbers.java
@@ -20,28 +20,32 @@ public class IdentifyRomanNumbers
     	PDDocument document = PDDocument.load(file);
     	PDFTextStripper pdfStripper = new PDFTextStripper();
     	String text = pdfStripper.getText(document);
-    	System.out.println(text);
     	
     	Pattern pattern =  Pattern.compile("([M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})&&[^\\p{Digit}]&&[^\\p{Punct}]]+\\.)");
     	Matcher matcher = pattern.matcher(text);
     	String contenido;
+    	String contenidoApartado;
 		int apuntador = 0;
+		
+		String nombreDeApartado;
+		String[] auxiliar;
 
 		while (matcher.find(apuntador)) {
-			//System.out.println("Apuntador 1: " + apuntador );
-			//System.out.println("Start: "+ matcher.start());
-			//System.out.println("End: "+ matcher.end());
 			apuntador = matcher.end();
+			
 			System.out.println("Apartado: "+ matcher.group());
-			contenido = text.substring(matcher.end()).trim();
-			System.out.println("Contenido: "+ contenido);
-			String[] spliteo = contenido.split("([M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})&&[^\\p{Digit}]&&[^\\p{Punct}]]+\\.)",2);
-			System.out.println("SPLIT 0: " + spliteo[0]);
-			System.out.println("SPLIT 1: " + spliteo[1]);
-			//System.out.println("Apuntador 2: " + apuntador );
-		}
+			auxiliar = matcher.group().toString().split("\\.",2);
+			nombreDeApartado = auxiliar[0].trim();
+			System.out.println("Nombre Apartado: " + nombreDeApartado);
 
-    	
+			contenido = text.substring(matcher.end()).trim();
+			String[] aux = contenido.split("([M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})&&[^\\p{Digit}]&&[^\\p{Punct}]]+\\.)");
+			auxiliar = contenido.split("([M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})&&[^\\p{Digit}]&&[^\\p{Punct}]]+\\.)",2);
+			contenidoApartado = auxiliar[0].trim();
+						
+			System.out.println("Contenido: "+ contenidoApartado);
+			System.out.println(" ");
+		}
     	
     	document.close();
     }
